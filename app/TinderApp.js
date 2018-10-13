ba7c8c35cdacb2b05c36d5c9b3f1306a41881392
pranaytanniru <pranay@betterhalf.ai>
2018-10-13 17:26:21 +0530

    added animation

--- app/TinderApp.js
+++ app/TinderApp.js
@@ -27,7 +27,7 @@ export default class TinderApp extends Component {
     this.state={
       currentIndex:0
     }
-    this.rotate=this.position.x.interpolate({
+    this.rotate=this.position.y.interpolate({
       inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
       outputRange:['-10deg','0deg','10deg'],
       extrapolate:'clamp'
@@ -49,6 +49,17 @@ export default class TinderApp extends Component {
       outputRange:[1,0,0],
       extrapolate:'clamp'
     })
+    this.nextCardOpacity=this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
+      outputRange:[1,0,1],
+      extrapolate:'clamp'
+    })
+    this.nextCardScale=this.position.x.interpolate({
+      inputRange:[-SCREEN_WIDTH/2,0,SCREEN_WIDTH/2],
+      outputRange:[1,0.8,1],
+      extrapolate:'clamp'
+    })
+
   }
   componentWillMount(){
     this.PanResponder=PanResponder.create({
@@ -57,13 +68,38 @@ export default class TinderApp extends Component {
         this.position.setValue({x:gestureState.dx,y:gestureState.dy})
       },
       onPanResponderRelease:(evt,gestureState)=>{
-
+        if(gestureState.dx>120){
+          Animated.spring(this.position,{
+            toValue:{x:SCREEN_WIDTH+100,y:gestureState.dy}
+          }).start(()=>{
+            this.setState({currentIndex:this.state.currentIndex+1},()=>{
+              this.position.setValue({x:0,y:0})
+            })
+          })
+        }
+        if(gestureState.dx<-120){
+          Animated.spring(this.position,{
+            toValue:{x:-SCREEN_WIDTH-100,y:gestureState.dy}
+          }).start(()=>{
+            this.setState({currentIndex:this.state.currentIndex+1},()=>{
+              this.position.setValue({x:0,y:0})
+            })
+          })
+        }
+        else{
+          Animated.spring(this.position,{
+            toValue:{x:0,y:0}
+          }).start()
+        }
       }
     })
   }
 
   _renderImages(){
     return Images.map((image,index)=>{
+      if(index<this.state.currentIndex){
+        return null
+      }
       if(index===this.state.currentIndex){
         return(
           <Animated.View
@@ -84,8 +120,11 @@ export default class TinderApp extends Component {
       }else{
         return(
           <Animated.View
+          {...this.PanResponder.panHandlers}
            key={index}
-           style={{height:SCREEN_HEIGHT-120,width:SCREEN_WIDTH,padding:10,position:'absolute'}}>
+           style={{opacity:this.nextCardOpacity,
+             transform:[{scale:this.nextCardScale}],
+             height:SCREEN_HEIGHT-120,width:SCREEN_WIDTH,padding:10,position:'absolute'}}>
             <Image
             style={{flex:1,height:null,width:null,resizeMode:'cover',borderRadius:20}}
             source={image.uri}/>
